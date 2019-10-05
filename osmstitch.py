cd005a0b974bb1b381cb001cc37999bcd2477d1d
Matus Tejiscak <ziman@functor.sk>
2019-10-05 23:47:40 +0200

    Support custom tileserver URL templates.

--- osmstitch.py
+++ osmstitch.py
@@ -7,6 +7,7 @@ import logging
 import argparse
 import requests
 import subprocess
+import urllib.parse
 from PIL import Image
 
 TILE_SIZE = 256
@@ -28,14 +29,17 @@ def deg2num(lat_deg, lon_deg, zoom):
 def div_roundup(x, y):
     return (x + y-1) // y
 
-def load_tile(dirname_cache, x, y, z):
+def load_tile(http, dirname_cache, url_template, x, y, z):
     log.debug('loading tile %s', (x, y, z))
-    fname = os.path.join(dirname_cache, f'{z}/{x}/{y}.png')
+
+    url = url_template.format(x=x, y=y, z=z)
+    url_parsed = urllib.parse.urlparse(url)
+    fname = os.path.join(dirname_cache, url_parsed.netloc, url_parsed.path.strip('/'))
 
     if not os.path.exists(fname):
         os.makedirs(os.path.dirname(fname), exist_ok=True)
-        resp = requests.get(
-            f'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        resp = http.get(
+            url,
             headers={'User-Agent': 'https://github.com/ziman/osmstitch'}
         )
         with open(fname, 'wb') as f:
@@ -73,10 +77,11 @@ def main(args):
     x_halfspan = div_roundup(width//2 - TILE_SIZE//2, TILE_SIZE)
     y_halfspan = div_roundup(height//2 - TILE_SIZE//2, TILE_SIZE)
 
+    http = requests.Session()
     result = Image.new('RGB', (width, height))
     for dx in range(-x_halfspan, x_halfspan + 1):
         for dy in range(-y_halfspan, y_halfspan + 1):
-            tile = load_tile(args.dirname_cache, x_centre + dx, y_centre + dy, args.zoom)
+            tile = load_tile(http, args.dirname_cache, args.url_template, x_centre + dx, y_centre + dy, args.zoom)
             result.paste(tile, (dx*TILE_SIZE - TILE_SIZE//2 + width//2, dy*TILE_SIZE - TILE_SIZE//2 + height//2))
 
     result.save(args.fname_out)
@@ -100,4 +105,6 @@ if __name__ == '__main__':
     ap.add_argument('-c', '--cache', dest='dirname_cache', default='cache/', help='cache directory [%(default)s]')
     ap.add_argument('-o', dest='fname_out', default='map.png', help='output filename [%(default)s]')
     ap.add_argument('-p', '--ppi', default=150, type=int, help='pixels per inch [%(default)s]')
+    ap.add_argument('-u', '--url-template', default='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        help='tile url template [%(default)s]')
     main(ap.parse_args())
